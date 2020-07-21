a501c719a6c7a932683064cc20ea1bfe33711a72
Ammar Shaikh <47348491+ammarshaikh123@users.noreply.github.com>
2020-07-20 20:46:12 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
-mkdir -p ~/.streamlit
-
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"sammar@tamu.edu\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
